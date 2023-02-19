f4068d2a4f4ed8218c26bd2c8fd71d5070998815
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-02-19 18:26:11 +0100

    persit data

--- src/main/java/com/history/backend/repositories/FieldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.history.backend.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FieldRepository extends MongoRepository<String, String> {
-
-}
